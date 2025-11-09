54a70d3b31d300bad2bfb171d9da92de762e24e9
kuuuube <hexagonisalie@gmail.com>
2025-11-09 12:19:06 -0500

    Implement IPenActionHandler for vmultimode output mode

--- src/OutputMode/VMultiMode/VMultiButtonHandler.cs
+++ src/OutputMode/VMultiMode/VMultiButtonHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Platform.Pointer;
 using OpenTabletDriver.Plugin.Tablet;
 using VoiDPlugins.Library.VMulti;
 using VoiDPlugins.Library.VoiD;
@@ -62,5 +63,37 @@ namespace VoiDPlugins.OutputMode
             _instance.DisableButtonBit(Bindings[Button!]);
             _instance.Write();
         }
+
+        public void Activate(PenAction action)
+        {
+            if (_instance == null)
+                return;
+
+            if (GetCode(action) is { } code)
+            {
+                _instance.EnableButtonBit(code);
+            }
+        }
+
+        public void Deactivate(PenAction action)
+        {
+            if (_instance == null)
+                return;
+
+            if (GetCode(action) is { } code)
+            {
+                _instance.DisableButtonBit(code);
+            }
+        }
+
+        private static int? GetCode(PenAction button) => button switch
+        {
+            PenAction.Tip => Bindings["Left"],
+            PenAction.Eraser => Bindings["Left"],
+            PenAction.BarrelButton1 => Bindings["Right"],
+            PenAction.BarrelButton2 => Bindings["Middle"],
+            PenAction.BarrelButton3 => null,
+            _ => null,
+        };
     }
 }
